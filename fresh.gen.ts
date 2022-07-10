39f082e369c9e1c620e8d02db0d8108ef748f592
Jared De Blander <jared0x90@gmail.com>
2022-07-09 21:31:57 -0400

    added /read route

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,6 +5,7 @@
 import * as $0 from "./routes/[name].tsx";
 import * as $1 from "./routes/api/joke.ts";
 import * as $2 from "./routes/index.tsx";
+import * as $3 from "./routes/read.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 import * as $$1 from "./islands/MealItem.tsx";
 
@@ -13,6 +14,7 @@ const manifest = {
     "./routes/[name].tsx": $0,
     "./routes/api/joke.ts": $1,
     "./routes/index.tsx": $2,
+    "./routes/read.tsx": $3,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
---
+++ routes/read.tsx
@@ -0,0 +1,10 @@
+/** @jsx h */
+import { h } from "preact";
+
+export default function Greet() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
