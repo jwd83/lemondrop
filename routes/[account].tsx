085b6b8545a30f6921f6440fd371351a308a6d99
Jared De Blander <jared@ayrscott.com>
2022-07-15 02:14:10 -0400

    adjusted props

--- routes/[account].tsx
+++ routes/[account].tsx
@@ -1,18 +1,15 @@
 /** @jsx h */
 import { h } from "preact";
-import { useState } from "preact/hooks";
-
 import { PageProps } from "$fresh/server.ts";
 import Header from "../islands/Header.tsx";
 import FuelForm from "../islands/FuelForm.tsx";
 import Account from "../islands/Account.tsx";
 
-export default function Greet(props: PageProps) {
-  const [account, setAccount] = useState("");
+export default function AccountHome(props: PageProps) {
   return (
     <div>
       <Header />
-      <Account account={account} />
+      <Account account={props.params.account} />
       <FuelForm />
     </div>
   );
